2e6f2a0fa9e04554fa3cb7a9200a5fbb1723ef0c
Adam Gardner <adam.gardner@dynatrace.com>
2024-12-27 15:29:49 +1000

    update

--- .devcontainer/testing/app.py
+++ .devcontainer/testing/app.py
@@ -1,33 +1,31 @@
 import subprocess
 
+def create_github_issue(output):
+    subprocess.run(["gh", "issue", "create", "--label", "e2e test failed", "--title", f"Failed on step: {step}", "--body", f"The end to end test script failed on step: {step}\n\n## Error\n```\n{output.stderr}\n```\n\n## Output\n```\n{output.stdout}\n```"])
+    exit(0)
+
 # Install Playwright for UI testing
-subprocess.run(["playwright", "install"])
+subprocess.run(["playwright", "install", "chromium", "chromium-headless-shell"])
 
 # npx --yes playwright install --with-deps
-with open("steps.txt", mode="r") as steps_file:
+with open(".devcontainer/testing/steps.txt", mode="r") as steps_file:
     steps = steps_file.readlines()
 
     for step in steps:
         step = step.strip()
+
         if step.startswith("//") or step.startswith("#"):
             print(f"Ignore this step. It is commented out: {step}")
             continue
         
-        output = []
         if "test_" in step:
             print(f"This step is a Playwright test: {step}")
-            output = subprocess.run(["pytest", "playwright"], capture_output=True, text=True)
+            output = subprocess.run(["pytest", f".devcontainer/testing/{step}"], capture_output=True, text=True)
+            if output.returncode != 0:
+                create_github_issue(output)
         else:
-            output = subprocess.run(["runme", "run", step], capture_output=True, text=True)
-
-        # If an error occurs during a step
-        # Create an issue in GitHub and exit
-        # Note: Something else is responsible for sending alerts to users based on issues
-        # This script simply opens the issue
-        if output.returncode != 0:
-            print(output)
-            stderr = output.stderr
-            stdout = output.stdout
-
-            subprocess.run(["gh", "issue", "create", "--label", "e2e test failed", "--title", f"Failed on step: {step}", "--body", f"The end to end test script failed on step: {step}\n\n## Error\n```\n{output.stderr}\n```\n\n## Output\n```\n{output.stdout}\n```"])
-            exit(0)
+            # Temp TODO remove
+            print("temporarily skipping runme tests")
+            #output = subprocess.run(["runme", "run", step], capture_output=True, text=True)
+            #if output.returncode != 0:
+            #    create_github_issue(output)
