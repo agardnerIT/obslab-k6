f4d9b94add3b0d1399ceb8616aa04146a7785064
Adam Gardner <26523841+agardnerIT@users.noreply.github.com>
2024-12-27 02:37:47 +0000

    update

--- .devcontainer/testing/app.py
+++ .devcontainer/testing/app.py
@@ -6,9 +6,6 @@ with open("steps.txt", mode="r") as steps_file:
     for step in steps:
 
         output = subprocess.run(["runme", "run", step.strip()], capture_output=True, text=True)
-        print("-- Step Output --")
-        print(output)
         if output.returncode != 0:
-            print("ERROR! Command Failed! Create an issue!")
             subprocess.run(["gh", "issue", "create", "--label", "e2e test failed", "--title", f"Failed on step {step.strip()}", "--body", f"The end to end test script failed on step: {step.strip()} with error: {output.stderr}"])
             exit(0)
