04f56ecca334d78d7d2edbbddeac792c3e2cac48
Adam Gardner <26523841+agardnerIT@users.noreply.github.com>
2024-12-27 03:56:21 +0000

    update

--- .devcontainer/testing/app.py
+++ .devcontainer/testing/app.py
@@ -1,7 +1,7 @@
 import subprocess
 
 # Install Playwright for UI testing
-#subprocess.run(["npx", "--yes", "playwright", "install", "chromium"])
+subprocess.run(["playwright", "install"])
 
 # npx --yes playwright install --with-deps
 with open("steps.txt", mode="r") as steps_file:
@@ -12,14 +12,22 @@ with open("steps.txt", mode="r") as steps_file:
         if step.startswith("//") or step.startswith("#"):
             print(f"Ignore this step. It is commented out: {step}")
             continue
-
-
-        output = subprocess.run(["runme", "run", step], capture_output=True, text=True)
+        
+        output = []
+        if "test_" in step:
+            print(f"This step is a Playwright test: {step}")
+            output = subprocess.run(["pytest", "playwright"], capture_output=True, text=True)
+        else:
+            output = subprocess.run(["runme", "run", step], capture_output=True, text=True)
 
         # If an error occurs during a step
         # Create an issue in GitHub and exit
         # Note: Something else is responsible for sending alerts to users based on issues
         # This script simply opens the issue
         if output.returncode != 0:
-            subprocess.run(["gh", "issue", "create", "--label", "e2e test failed", "--title", f"Failed on step: {step}", "--body", f"The end to end test script failed on step: {step.strip()} with error: {output.stderr}"])
+            print(output)
+            stderr = output.stderr
+            stdout = output.stdout
+
+            subprocess.run(["gh", "issue", "create", "--label", "e2e test failed", "--title", f"Failed on step: {step}", "--body", f"The end to end test script failed on step: {step}\n\n## Error\n```\n{output.stderr}\n```\n\n## Output\n```\n{output.stdout}\n```"])
             exit(0)
