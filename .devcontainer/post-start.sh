78155591e79a5e36f0b0e3bdefddbdceedaa8c1f
Adam Gardner <26523841+agardnerIT@users.noreply.github.com>
2024-12-27 02:35:16 +0000

    update

--- .devcontainer/post-start.sh
+++ .devcontainer/post-start.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Set default repository for gh CLI
+# Required for the e2e test harness
+# If it needs to interact with GitHub (eg. create an issue for a failed e2e test)
+gh repo set-default $GITHUB_REPOSITORY
+
 # Startup Ping
 # curl -X POST https://grzxx1q7wd.execute-api.us-east-1.amazonaws.com/default/codespace-tracker \
 #   -H "Content-Type: application/json" \
