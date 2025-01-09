abd6254b684e650e93a69d5295587b81bb510721
Adam Gardner <adam.gardner@dynatrace.com>
2025-01-09 12:40:51 +1000

    update

--- .devcontainer/testing/app.py
+++ .devcontainer/testing/app.py
@@ -1,4 +1,8 @@
 import subprocess
+import os
+
+DEV_MODE = os.environ.get("DEV_MODE", "FALSE").upper() # This is a string. NOT a bool.
+print(f"DEV_MODE: {DEV_MODE}")
 
 def create_github_issue(output):
     subprocess.run(["gh", "issue", "create", "--label", "e2e test failed", "--title", f"Failed on step: {step}", "--body", f"The end to end test script failed on step: {step}\n\n## Error\n```\n{output.stderr}\n```\n\n## Output\n```\n{output.stdout}\n```"])
@@ -21,10 +25,14 @@ with open("steps.txt", mode="r") as steps_file:
         if "test_" in step:
             print(f"This step is a Playwright test: {step}")
             output = subprocess.run(["pytest", f"./{step}"], capture_output=True, text=True)
-            if output.returncode != 0:
+            if output.returncode != 0 and DEV_MODE == "FALSE":
                 create_github_issue(output)
+            else:
+                print(output)
         else:
             output = subprocess.run(["runme", "run", step], capture_output=True, text=True)
             print(f"[{step}] | {output.returncode} | {output.stdout}")
-            if output.returncode != 0:
+            if output.returncode != 0 and DEV_MODE == "FALSE":
                 create_github_issue(output)
+            else:
+                print(output)
